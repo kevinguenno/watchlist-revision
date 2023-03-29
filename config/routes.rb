aa240a1321a1f1434716cc7eede7926b851229d4
kevinguenno <kevin.guenno@gmail.com>
2023-03-29 10:25:47 +0200

    lists controller et view index show new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "lists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists
 end
